8256710781620628ecc3564c9bb3b1474e80c132
ivan <rovedabr@gmail.com>
2023-02-09 15:43:32 -0300

    knex file

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const routes = require("./routes")
 
 const PORT = 5555;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
