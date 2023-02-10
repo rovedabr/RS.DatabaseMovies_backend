78e954f27db6aaed9cfdbbb78da93499109e6072
ivan <rovedabr@gmail.com>
2023-02-10 15:18:52 -0300

    users routes created

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,16 @@
 const { hash, compare } = require("bcrypt")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
+
+class UserController {
+  async create(request, response) {
+    const { name, email, password } = request.body
+    const database = await sqliteConnection()
+    const hashedPassword = await hash(password, 10)
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+    return response.status(201).json()
+  }
+}
+
+module.exports = UserController
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
-const routes = require("./routes")
+// const routes = require("./routes")
+const AppError = require("./utils/AppError")
+
+app.use(express.json())
+// app.use(routes)
+
 
 const PORT = 5555;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
